068a0e73102bd055cdb38f8866cae7e86748bcdb
Tobias Jansson <tobias.janssn@gmail.com>
2023-09-25 16:37:31 +0200

    meny print och enkel sökfunktion

--- elias_klass.py
+++ elias_klass.py
@@ -1,5 +1,6 @@
 class vara:
-    # Specifierade 'float' för pris så att man inte försökte räkna ut med sträng eller dylikt
+    # Specifierade 'float' för pris så att man inte försökte räkna ut med
+    # sträng eller dylikt
     def __init__(self, namn, varukod, pris: float):
         self.namn = namn
         self.varukod = varukod
@@ -9,12 +10,14 @@ class vara:
         # Ändrade utskriften för att snygga till utskriften av ordern
         return f" ({self.varukod}) {self.namn}: {self.pris} kr"
 
+
 class Order(list):
     '''
-    Genererar ett ordernummer och ett totalpris 
+    Genererar ett ordernummer och ett totalpris
     för en lista med objekt av typen vara.
     '''
     next_order_no = 1
+
     def __init__(self, varor: list):
         self.order_no = Order.next_order_no
         Order.next_order_no += 1
@@ -29,13 +32,14 @@ class Order(list):
         order_info += f"Pris: {self.tot_pris}"
         return order_info
 
-#Exempel på användning av klasserna:
+# Exempel på användning av klasserna:
 
-vara1 = vara("Produkt 1", "P001", 100)
-vara2 = vara("Produkt 2", "P002", 150)
-vara3 = vara("Produkt 3", "P003", 75)
 
-varor_i_ordern = [vara1, vara2, vara3]
+# vara1 = vara("Produkt 1", "P001", 100)
+# vara2 = vara("Produkt 2", "P002", 150)
+# vara3 = vara("Produkt 3", "P003", 75)
 
-order = Order(varor_i_ordern)
-print(order)
+# varor_i_ordern = [vara1, vara2, vara3]
+
+# order = Order(varor_i_ordern)
+# print(order)
--- tobias.py
+++ tobias.py
@@ -1,5 +1,5 @@
 import pickle
-import tobiasclass
+import elias_klass
 
 # Laddar objekt sparade i picklesave.txt
 try:
@@ -10,11 +10,54 @@ except EOFError:
 except FileNotFoundError:
     lst_varor = []
 
+try:
+    with open("ordersave.txt", "rb") as g:
+        lst_orders = pickle.load(g)
+except EOFError:
+    lst_orders = []
+except FileNotFoundError:
+    lst_orders = []
+
 
 # metod för att lägga till en ny vara
 def nyvara(namn, varukod, pris):
-    nyvara = tobiasclass.vara(namn, varukod, pris)
+    nyvara = elias_klass.vara(namn, varukod, pris)
     lst_varor.append(nyvara)
+
+
+# ny_vara1 = elias_klass.vara("vara1choklad", "P001", 100)
+# ny_vara2 = elias_klass.vara("vara2", "P002", 250)
+# ny_vara3 = elias_klass.vara("vara3banan", "P003", 50)
+# ny_vara4 = elias_klass.vara("vara4", "P004", 50)
+# ny_vara5 = elias_klass.vara("vara5blabla", "P005", 50)
+
+# lst_varor = [ny_vara1, ny_vara2, ny_vara3, ny_vara4, ny_vara5]
+def sell():
+    new_order = elias_klass.Order(lst_varor, len(lst_orders))
+    lst_orders.append(new_order)
+
+
+def meny(bredd):
+    print(f"| {'Café Python'} |".center(bredd))
+    print("*"*bredd)
+    toprint = ""
+    for x in range(1, len(lst_varor) + 1):
+        if x % 3 != 0:
+            toprint += f"│{lst_varor[x - 1]}".ljust(int(bredd/3))
+        else:
+            toprint += f"│{lst_varor[x - 1]}│\n".rjust(int(bredd/3))
+    print(toprint + "\n")
+    print("*" * bredd)
+    print("-" * bredd)
+    print("A | Ny order")
+    print("B | Ta bort vara")
+    print("C | Retur")
+    print("-" * bredd)
+
+
+meny(90)
+i = int(input("Search for ordernumber: "))
+print(lst_orders[i])
 
 
 # Lite exempelkod för att testa
@@ -25,3 +68,6 @@ def nyvara(namn, varukod, pris):
 # Sparar alla objekt till picklesave.txt
 with open("picklesave.txt", "wb") as f:
     pickle.dump(lst_varor, f)
+
+with open("ordersave.txt", "wb") as g:
+    pickle.dump(lst_orders, g)
